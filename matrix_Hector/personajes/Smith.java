7843765e24e6f96f4075bcb8b8fc4b839911168a
Hector Lopez <hlopdia699@g.educaand.es>
2023-04-11 13:45:24 +0200

    matrix

--- matrix_Hector/personajes/Smith.java
+++ matrix_Hector/personajes/Smith.java
@@ -13,13 +13,16 @@ public class Smith extends Personaje{
         this.nombre = "SMITH";
 
         infeccion = (int)(Math.random()*3)+1;
+        iteracionDeCreaccion = 0;
     }
+
+
 
     // METODOS:
     @Override
     public String mostrarInformacion()
     {
-        return "Smith-> id: " + super.getId() + ", nombre: " + super.getNombre() + ", ciudad: " + super.getCiudad();
+        return super.getId() + ", nombre: " + super.getNombre() + ", ciudad: " + super.getCiudad();
     }
 
     public int getInfeccion() {
@@ -30,4 +33,9 @@ public class Smith extends Personaje{
         return iteracionDeCreaccion;
     }
 
+    public void setIteracionDeCreaccion(int iteracionDeCreaccion) {
+        this.iteracionDeCreaccion = iteracionDeCreaccion;
+    }
+
+    
 }
