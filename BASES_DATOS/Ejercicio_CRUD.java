bf5f278097eada2640d714ce2db459c1b2a8c580
Hector Lopez <hlopdia699@g.educaand.es>
2023-04-12 12:57:00 +0200

    BasesDeDatos

---
+++ BASES_DATOS/Ejercicio_CRUD.java
@@ -0,0 +1,271 @@
+package BASES_DATOS;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Ejercicio_CRUD {
+
+    static Scanner sc = new Scanner(System.in);
+
+
+    // FINALS USADOS PARA EL ESTILO DEL PROGRAMA:
+    static final String ANSI_GREEN_BG = "\u001B[42m";
+    static final String ANSI_WHITE_BG = "\u001B[47m";
+    static final String ANSI_RED_BG = "\u001B[41m";
+    static final String ANSI_GREEN = "\u001B[32m";
+    static final String ANSI_RED = "\u001B[31m";
+    static final String ANSI_RESET = "\u001B[0m";
+
+
+    public static void main(String[] args) {
+
+        // VARIABLES CONEXION CON BASE DE DATOS:
+        Connection conexion;
+        System.setProperty("jdbc.drivers", "com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/people";
+        String user = "admin";
+        String pass = "admin";
+
+        
+        try
+        {
+        conexion = DriverManager.getConnection(url ,user, pass);
+        System.out.println("Conexión realizada con éxito.");
+
+
+        // VARIABLES BUCLE:
+        boolean salir = false;
+        int opcion = 0;
+
+        // BUCLE MENU:
+        do
+        {
+            opcion = menu();
+            switch(opcion)
+            {
+                case 1:
+                    consulta(conexion);
+                    break;
+                case 2:
+                    alta(conexion);
+                    break;
+                case 3:
+                    baja(conexion);
+                    break;
+                case 4:
+                    modificaDatos(conexion);
+                    break;
+                case 0:
+                    salir = true;
+                    System.out.println(ANSI_WHITE_BG + "\t\tHasta pronto\t\t" + ANSI_RESET);
+                    break;
+                default:
+                    System.out.println("Opción incorrecta, vuelve a intentarlo");
+            }
+            System.out.println();
+
+        }while(!salir); // FIN BUCLE
+
+        // Cierro la conexión al salir del programa:
+        conexion.close();
+        System.out.println("Conexión cerrada correctamente.");
+
+        }
+        catch (SQLException sqle)
+        {
+        System.out.println(ANSI_RED_BG + sqle.getErrorCode() + " -> " + sqle.getMessage() + ANSI_RESET);
+        }
+    }
+
+
+    /****** FUNCIONES: ********/
+
+    /**
+     * Función que muestra el menú de opciones y recoge y devuelve un entero 
+     * con la opción que utilizará el switch
+     * @return opcion
+     */
+    static int menu()
+    {
+        System.out.println("\n\t" + ANSI_GREEN_BG + "BIENVENIDO AL MENU DE CLIENES" + ANSI_RESET + "\n");
+        System.out.println("1 - Consulta de datos");
+        System.out.println("2 - Alta nueva");
+        System.out.println("3 - Baja de persona");
+        System.out.println("4 - Modificar datos");
+        System.out.println("0 - Salir");
+        System.out.println("---------------------------------------------");
+        System.out.println(" Indique una opción:\n");
+
+        int opcion = sc.nextInt();
+        sc.nextLine();
+        return opcion;
+    }
+
+    static int submenu()
+    {
+        System.out.println("\n\t" + ANSI_GREEN_BG + "SUBMENÚ DE EDICIÓN\nSeleccione el campo a editar:" + ANSI_RESET + "\n");
+        System.out.println("1 - Nombre");
+        System.out.println("2 - Apellidos");
+        System.out.println("3 - Teléfono");
+        System.out.println("0 - Salir");
+        System.out.println("---------------------------------------------");
+        System.out.println(" Indique una opción:\n");
+
+        int opcion = sc.nextInt();
+        sc.nextLine();
+        return opcion;
+    }
+
+    /**
+     * Realiza la consulta de un campo de la base de datos por DNI o por teléfono
+     * @param conexion
+     */
+    static void consulta(Connection conexion)
+    {
+        String consulta = "select nombre, apellidos, telefono from people where dni = ";
+        System.out.println("Inserta el DNI de la persona:");
+        String dniBuscado = sc.nextLine();
+        consulta = consulta + "'" + dniBuscado + "'";
+        
+        try
+        { 
+            Statement st;
+            st=conexion.createStatement();
+            ResultSet rs;
+            rs=st.executeQuery(consulta);
+            rs.next();
+
+            String nombre = rs.getString("nombre");
+            String apellidos = rs.getString("apellidos");
+            String telefono = rs.getString("telefono");
+            System.out.println("\n\nPersona encontrada:");
+            System.out.println("nombre -> " + nombre);
+            System.out.println("apellidos -> " + apellidos);
+            System.out.println("telefono -> " + telefono);
+        }
+        catch (SQLException sqle)
+        {
+        System.out.println(ANSI_RED_BG + sqle.getErrorCode() + " -> " + sqle.getMessage() + ANSI_RESET);
+        }
+    }
+
+    /**
+     * Da de alta un nuevo registro en la base de datos
+     * @param conexion
+     */
+    static void alta(Connection conexion)
+    {
+        String consulta = "insert into people values(";
+
+        System.out.println("\nInserta el DNI de la persona:");
+        String dni = sc.nextLine();
+
+        System.out.println("Inserta el nombre de la persona:");
+        String nombre = sc.nextLine();
+
+        System.out.println("Inserta el apellido de la persona:");
+        String apellido = sc.nextLine();
+
+        System.out.println("Inserta el teléfono de la persona:");
+        String telefono = sc.nextLine();
+
+        consulta = consulta + "'" + dni + "','" + nombre + "','" + apellido + "','" + telefono + "')";
+        
+        try
+        { 
+            Statement st;
+            st=conexion.createStatement();
+            st.executeUpdate(consulta);
+
+            System.out.println(ANSI_GREEN + "Usuario creado correctamente" + ANSI_RESET);
+        }
+        catch (SQLException sqle)
+        {
+        System.out.println(ANSI_RED_BG + sqle.getErrorCode() + " -> " + sqle.getMessage() + ANSI_RESET);
+        }
+    }
+
+    /**
+     * Borra un registro de la base de datos buscando por el DNI
+     * @param conexion
+     */
+    static void baja(Connection conexion)
+    {
+        String consulta = "delete from people where dni = ";
+        System.out.println("Inserta el DNI de la persona:");
+        String dniBuscado = sc.nextLine();
+        consulta = consulta + "'" + dniBuscado + "'";
+        
+        try
+        { 
+            Statement st;
+            st=conexion.createStatement();
+            st.executeUpdate(consulta);
+
+            System.out.println(ANSI_GREEN + "Usuario borrado correctamente" + ANSI_RESET);
+        }
+        catch (SQLException sqle)
+        {
+        System.out.println(ANSI_RED_BG + sqle.getErrorCode() + " -> " + sqle.getMessage() + ANSI_RESET);
+        }
+    }
+
+    static void modificaDatos(Connection conexion)
+    {
+
+        String consultaInicial = "select nombre, apellidos, telefono from people where dni = ";
+        System.out.println("Inserta el DNI de la persona:");
+        String dniBuscado = sc.nextLine();
+        consultaInicial = consultaInicial + "'" + dniBuscado + "'";
+     
+        try
+        {
+            Statement st;
+            st=conexion.createStatement();
+            ResultSet rs;
+            rs=st.executeQuery(consultaInicial);
+            rs.next();
+
+            String nombre = rs.getString("nombre");
+            String apellidos = rs.getString("apellidos");
+            String telefono = rs.getString("telefono");
+
+            String consulta = "update people set nombre='MarceloDuarte', apellidos='Marce' where dni='Marcelo'";
+            String consulta2 = "where dni='" + dniBuscado + "'";
+
+
+            System.out.println("Inserta el nombre de la persona:");
+            String nombreNuevo = sc.nextLine();
+            if(nombreNuevo.length()==0)
+            {
+                nombreNuevo = nombre;
+            }
+    
+            System.out.println("Inserta el apellido de la persona:");
+            String apellidoNuevo = sc.nextLine();
+            if(apellidoNuevo.length()==0)
+            {
+                apellidoNuevo = apellidos;
+            }
+    
+            System.out.println("Inserta el teléfono de la persona:");
+            String telefonoNuevo = sc.nextLine();
+            if(telefonoNuevo.length()==0)
+            {
+                telefonoNuevo = telefono;
+            }
+
+            consulta = consulta + "'" + dni + "','" + nombre + "','" + apellido + "','" + telefono + "')";
+
+            System.out.println(ANSI_GREEN + "Usuario modificado correctamente" + ANSI_RESET);
+
+        }
+        catch (SQLException sqle)
+        {
+        System.out.println(ANSI_RED_BG + sqle.getErrorCode() + " -> " + sqle.getMessage() + ANSI_RESET);
+        }
+    }
+
+
+
+}
