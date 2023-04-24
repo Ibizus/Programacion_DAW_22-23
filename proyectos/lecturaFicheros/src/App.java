b6e0e855e8a71d2e26bc3be7a352dccfd213205e
Hector Lopez <hlopdia699@g.educaand.es>
2023-04-24 08:33:23 +0200

    ficheros

--- proyectos/lecturaFicheros/src/App.java
+++ proyectos/lecturaFicheros/src/App.java
@@ -1,7 +1,42 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
 
-        
+        System.out.println("Leo con FileReader");
+        FileReader fr = null;
+    
+        try 
+        {
+            fr = new FileReader("src/recursos/fichero1.txt");
+
+            int caracterLeido = fr.read();
+
+            while (caracterLeido != -1)
+            {
+                System.out.println((char)caracterLeido);
+                caracterLeido = fr.read();
+            }
+            
+        }
+        catch (FileNotFoundException e1)
+        {
+            System.out.println(e1.getMessage());
+        }
+        catch (Exception e) 
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            if(fr != null)
+            {
+                fr.close();
+            }
+        }
+
+
     }
 }
