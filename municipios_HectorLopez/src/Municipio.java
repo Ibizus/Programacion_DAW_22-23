00c7c5fcf4851e80a25f8f9732a07df26b1c9c0a
Hector Lopez <hlopdia699@g.educaand.es>
2023-04-28 12:01:38 +0200

    municipios

---
+++ municipios_HectorLopez/src/Municipio.java
@@ -0,0 +1,47 @@
+public class Municipio implements Comparable<Municipio>{
+    
+
+    // ATRIBUTOS:
+    private String cod_postal;
+    private String nombre;
+    private String año;
+    private int poblacion;
+
+
+    // CONSTRUCTOR:
+    public Municipio(String cp, String nombre, String año, int poblacion)
+    {
+        this.cod_postal = cp;
+        this.nombre = nombre;
+        this.año = año;
+        this.poblacion = poblacion;
+    }
+
+    // METODOS:
+    @Override
+    public int compareTo(Municipio otro)
+    {
+        int dif = this.nombre.compareTo(otro.nombre);
+
+        if(dif == 0)
+        {
+            dif = Integer.valueOf(this.año) - Integer.valueOf(otro.año);
+        }
+
+        return dif;
+    }
+
+    @Override
+    public boolean equals(Object otro)
+    {
+        boolean iguales = false;
+
+        if(this.compareTo((Municipio)otro) == 0)
+        {
+            iguales = true;
+        }
+
+        return iguales;
+    }
+    
+}
