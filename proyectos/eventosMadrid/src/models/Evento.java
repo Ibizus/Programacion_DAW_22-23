7376803418af396f86ee4d441e0bd3907fa21ca1
Hector Lopez <hector.ldz@gmail.com>
2023-05-21 23:40:18 +0200

    eventos

---
+++ proyectos/eventosMadrid/src/models/Evento.java
@@ -0,0 +1,18 @@
+package models;
+
+public class Evento {
+    
+
+    // ATTRIBUTES:
+    private String id;
+    private String title;
+    private String description;
+    private boolean free;
+    private double price;
+    private String dtstart;
+    private String dtend;
+    private String time;
+    private String excluded_days;
+    private String audience;
+    
+}
